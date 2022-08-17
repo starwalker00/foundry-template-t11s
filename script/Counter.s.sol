4a9e662f785b398a70a3087eeb42a19fda532c45
starwalker00 <aaronwalkerup@gmail.com>
2022-08-17 22:35:32 +0200

    feat: improve deploy script

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -4,10 +4,21 @@ pragma solidity 0.8.15;
 import "forge-std/Script.sol";
 import "../src/Counter.sol";
 
+error NotEnoughBalance(address owner, uint256 balance);
+
 contract MyScript is Script {
     function run() external {
         vm.startBroadcast();
 
+        // get balance
+        address deployer = 0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045;
+        uint256 balance = deployer.balance;
+        console.log("balance::");
+        console.log(balance);
+        if(!(balance > 0))
+            revert NotEnoughBalance({owner: deployer, balance: balance});
+
+        // deploy contract
         Counter counter = new Counter();
 
         vm.stopBroadcast();
